b81e054837774f2bda754177c471234c920d31b0
bytedown <igor.yuzovitskiy@gmail.com>
2016-06-02 16:50:11 -0400

    Change js file

--- js/main.js
+++ js/main.js
@@ -4,12 +4,25 @@ $(document).ready(function(){
     arrows: true
   });
   openTextBox();
-  mixpanel.track("Video play");
+  // Mixpanel Events
+  pageView();
+  textButtonClick()
 });
 
 function openTextBox() {
   $(".number").click(function(){
     window.location = $(this).find("a").attr("href");
     return false;
+  });
+}
+
+// Mixpanel Events
+function pageView() {
+  mixpanel.track("Page Viewed");
+}
+
+function textButtonClick() {
+  $("number").on("click", function() {
+    mixpanel.track("Clicked Text Button");
   });
 }
