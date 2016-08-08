8c6851e5afe31631d3e661040158f3d4cb832d9e
bytedown <igor.yuzovitskiy@gmail.com>
2016-08-08 14:59:29 -0400

    Fix navbar padding.

--- js/overlay.js
+++ js/overlay.js
@@ -77,7 +77,7 @@ function setupOrderLater() {
 function fillInDatePicker() {
   var minTime = new Date();
   minTime.setMinutes(minTime.getMinutes() + 15);
-  var formatedTime = minTime.toLocaleTimeString().replace(/:\d+ /, ' ').replace(/\s*\(?EDT\)?/, '');
+  var formatedTime = minTime.toLocaleTimeString().replace(/:\d+ /, ' ');
   $('.time-picker').val( formatedTime );
 }
 
