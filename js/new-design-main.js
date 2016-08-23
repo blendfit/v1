a256d6765e69723835594fd6c9f7ce9c4ef3a9f9
DeProdigy <alex.hint.ah@gmail.com>
2016-08-23 13:32:10 -0400

    Sticky navbar button

---
+++ js/new-design-main.js
@@ -0,0 +1,8 @@
+$( document ).ready( function(){
+  $( '.spark-learn-more-button' ).sticky({
+    topSpacing: 20,
+    center: true,
+    responsiveWidth: true,
+    widthFromWrapper: true
+  });
+});
