3b1b105eebf47669e606a23dab97a9bb5d50cdf8
DeProdigy <alex.hint.ah@gmail.com>
2016-08-27 00:07:07 -0400

    Fix Landing Page

--- js/new-design-main.js
+++ js/new-design-main.js
@@ -1,10 +1,10 @@
 $( document ).ready( function(){
-  // $( '.spark-learn-more-button' ).sticky({
-  //   topSpacing: 20,
-  //   center: true,
-  //   responsiveWidth: true,
-  //   widthFromWrapper: true
-  // });
+  $( '.spark-learn-more-button' ).sticky({
+    topSpacing: 20,
+    center: true,
+    responsiveWidth: true,
+    widthFromWrapper: true
+  });
 
-  // $(".new-design-landing-page").css("height", $(document).height() + "px");
+  $(".new-design-landing-page").css("height", $(window).height() + "px");
 });
