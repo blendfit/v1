6ce19e268847335d5d9c778597e0718ebe51154b
bytedown <igor.yuzovitskiy@gmail.com>
2016-09-14 17:21:55 -0400

    Change landing-page copy.

--- js/new-design-main.js
+++ js/new-design-main.js
@@ -1,27 +1,17 @@
-$( document ).ready( function(){
-  // $( '.spark-learn-more-button' ).sticky({
-  //   topSpacing: 0,
-  //   widthFromWrapper: true
-  // });
-
-  // $(".spark-learn-more-button").stick_in_parent(
-  //   {
-  //     parent: $('body')
-  //   }
-  // );
+$(document).ready(function(){
 
   $(".new-design-landing-page").css("height", $(window).height() + "px");
-
+  
   var lastScrollTop = 0,
       $nav = $(".sticky-nav"),
       $page = $(".new-design-landing-page"),
       $stickyNavContainer = $(".sticky-nav-container");
-
+      
   $(window).on("scroll", function(e) {
+    
     var st = $(this).scrollTop();
-     if ( st > lastScrollTop ){
+     if (st > lastScrollTop){
          // downscroll code
-
          if( $page.height() < (lastScrollTop) ) {
            $nav.addClass('filled-in');
            $stickyNavContainer.removeClass('hidden');
@@ -34,9 +24,7 @@ $( document ).ready( function(){
           $stickyNavContainer.addClass('hidden');
         }
      }
-
      lastScrollTop = st;
   });
-
-
+  
 });
